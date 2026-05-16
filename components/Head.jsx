988d3952ca4fe708f6a9d7b9a5d3343184adae11
Jaly <jalyjaly306@gmail.com>
2026-05-16 07:22:17 +0700

    add and correc the name of Entry and Head

---
+++ components/Head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+        <header>
+            <nav>
+                <img src="../images/react-logo.png" alt="React logo" />
+                <span>My Travel Jounal</span>
+            </nav>
+        </header>
+    )
+}
